2b459d3bfe50b5ce25d56680107970710840f9e8
Abigail Endris <endrisabigail@gmail.com>
2026-07-16 14:24:15 -0500

    debugging deployment error

--- backend/server/config/firebaseAdmin.js
+++ backend/server/config/firebaseAdmin.js
@@ -3,6 +3,7 @@ import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
 import path from "path";
 
+const { credential, initializeApp } = pkg;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
@@ -10,8 +11,8 @@ const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
     : JSON.parse(
         readFileSync(path.join(__dirname, "firebaseServiceAccount.json"), "utf8"),
     );
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+initializeApp({
+    credential: credential.cert(serviceAccount),
 });
 
-export default admin;
+export default pkg;
