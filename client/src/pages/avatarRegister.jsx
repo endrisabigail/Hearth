5c7f5f1c6607479ff58fdfb704eb86212507d471
Abigail Endris <endrisabigail@gmail.com>
2026-07-28 10:10:43 -0500

    opacity of shadow

--- client/src/pages/avatarRegister.jsx
+++ client/src/pages/avatarRegister.jsx
@@ -267,7 +267,7 @@ function AvatarRegister() {
           new THREE.MeshBasicMaterial({
             color: 0x000000,
             transparent: true,
-            opacity: 0.15,
+            opacity: 0.5,
             depthWrite: false,
           }),
         );
